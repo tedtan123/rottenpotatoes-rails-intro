309cc908e4e1de828a0a089b5e52e367f1f11d91
EC2 Default User <ec2-user@ip-172-31-44-62.us-east-2.compute.internal>
2019-02-17 17:26:59 +0000

    part1 test 4

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,11 +2,11 @@ class Movie < ActiveRecord::Base
     # Code below Feb 16
   def order_list(sort_by)
     if sort_by =='title'
-      Movie.order(title: :asc)
+      self.movies.order(title: :asc)
     elsif sort_by == 'release_date'
-      Movie.order(release_date: :asc)
+      self.movies.order(release_date: :asc)
     else
-      Movie.order(created_at: :asc)
+      self.movies.order(created_at: :asc)
     end
   end
 # Code above Feb 16  
